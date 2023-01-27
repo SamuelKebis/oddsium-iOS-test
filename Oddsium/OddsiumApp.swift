c7628a363f55ce579bfc2cdb9b280036713917c3
Samuel Kebis <samuel.kebis@shortcut.no>
2023-01-27 09:46:23 +0100

    Initial commit

---
+++ Oddsium/OddsiumApp.swift
@@ -0,0 +1,17 @@
+//
+//  OddsiumApp.swift
+//  Oddsium
+//
+//  Created by Samuel Kebis on 27/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct OddsiumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
