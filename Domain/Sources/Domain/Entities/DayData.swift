5b03e36a3e7d01d239dcaf8031d325cd1c4834f6
Samuel Kebis <samuel.kebis@shortcut.no>
2023-01-30 11:20:40 +0100

    Added entities needed for football-games request

--- Domain/Sources/Domain/Entities/DayData.swift
+++ Domain/Sources/Domain/Entities/DayData.swift
@@ -1,8 +1,12 @@
 //
-//  File.swift
+//  DayData.swift
 //  
 //
 //  Created by Samuel Kebis on 30/01/2023.
 //
 
 import Foundation
+
+public struct DayData {
+    let data: DayMatches
+}
---
+++ Domain/Sources/Domain/Entities/Score.swift
@@ -0,0 +1,14 @@
+//
+//  Score.swift
+//  
+//
+//  Created by Samuel Kebis on 30/01/2023.
+//
+
+import Foundation
+
+public struct Score {
+    public let htScore: String
+    public let atScore: String
+    public let scoreDetails: String
+}
