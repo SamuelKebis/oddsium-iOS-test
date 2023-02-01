d6c8c39ff33b6cdc1b1662b0c3dfa45234703d3e
Samuel Kebis <samuel.kebis@shortcut.no>
2023-02-01 09:28:21 +0100

    Added date format for api requests

--- Domain/Sources/Domain/Extensions/Date+.swift
+++ Domain/Sources/Domain/Extensions/Date+.swift
@@ -19,6 +19,9 @@ public extension Date {
         case dayOfWeek
         /// Example: "Nov 12"
         case monthAndDay
+
+        // Example: "2023-02-06"
+        case apiRequest
     }
 
     func asString(_ format: AsStringFormat) -> String {
@@ -36,6 +39,11 @@ public extension Date {
         case .monthAndDay:
             let formatter = DateFormatter()
             formatter.dateFormat = "MMM d"
+            return formatter.string(from: self)
+
+        case .apiRequest:
+            let formatter = DateFormatter()
+            formatter.dateFormat = "yyyy-MM-dd"
             return formatter.string(from: self)
         }
     }
